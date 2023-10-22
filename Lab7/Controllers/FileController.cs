cf61e7c7f2ba3ffd3ecf6aaf4fb9985fe8b61414
victo <victo@LAPTOP-GHDR81TK>
2023-10-22 14:46:58 +0200

    Add project files.

---
+++ Lab7/Controllers/FileController.cs
@@ -0,0 +1,28 @@
+﻿using Lab7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace Lab7.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(FileDownloadModel model)
+        {
+            string content = $"First Name: {model.FirstName}\nLast Name: {model.LastName}";
+
+            byte[] bytes = Encoding.ASCII.GetBytes(content);
+
+            // Використовуйте назву файлу з моделі або за замовчуванням, якщо вона не вказана
+            string fileName = string.IsNullOrEmpty(model.FileName) ? "default.txt" : $"{model.FileName}.txt";
+
+            return File(bytes, "text/plain", fileName);
+        }
+    }
+}
---
+++ Lab7/Model/FileDownloadModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab7.Models
+{
+    public class FileDownloadModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+    }
+}
+
